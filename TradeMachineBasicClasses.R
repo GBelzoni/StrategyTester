4fd70f7d703d60f91cb3becbdffe14bc8c579905
GBelzoni <patrickhcostello@gmail.com>
2012-10-14 13:01:40 +0300

    runStrategy working

--- TradeMachineBasicClasses.R
+++
@@ -1,8 +0,0 @@
-source('TradeStrategyClasses.R')
-  TS1##Classes for Strategy tester
-#Need Trade Classes
-source("TradeClasses.R")
-par(savepar)##Classes for Strategy tester
-  #Trade Classes - Requires MarketDataClasses
-source("MarketDataClasses.R")
-source("PortfolioClasses.R")
